12ba7ab009ebeafb587e68a84476ae6fd4eef196
Mitchel Vostrez <mitch@voz.dev>
2025-11-24 11:26:23 -0600

    anotha one

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,7 +1,7 @@
 import { createAnthropic } from '@ai-sdk/anthropic'
+import { createGoogleGenerativeAI } from '@ai-sdk/google'
 import { createOpenAI } from '@ai-sdk/openai'
 import { createVercel } from '@ai-sdk/vercel'
-import { createGoogleGenerativeAI } from '@ai-sdk/google'
 
 /**
  * Supported language model providers.
